4978ee5c7b4d2a884c02f04c017a5ce3398f2271
Artem <gusev2014russia@mail.ru>
2019-07-13 20:53:49 +0300

    create local server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+let server = http.createServer((req, res)=>{
+   console.info(`URL current page: ${req.url}`);
+   res.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
+   res.end('Hello world!');
+});
+
+server.listen(8000, '127.0.0.1');
+console.info('We listening port 8000');

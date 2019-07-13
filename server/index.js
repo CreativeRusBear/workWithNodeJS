e510459b142567bdb43153685ff3b0d1bac58c0e
Artem <gusev2014russia@mail.ru>
2019-07-13 20:54:46 +0300

    change spaces in this file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
-let server = http.createServer((req, res)=>{
-   console.info(`URL current page: ${req.url}`);
-   res.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
-   res.end('Hello world!');
+let server = http.createServer((req, res) => {
+    console.info(`URL current page: ${req.url}`);
+    res.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
+    res.end('Hello world!');
 });
 
 server.listen(8000, '127.0.0.1');

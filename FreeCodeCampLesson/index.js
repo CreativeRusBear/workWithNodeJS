514149b4d9d5b3db5fb64966c4f9ab11c7720bc3
Artem <gusev2014russia@mail.ru>
2019-08-31 18:27:42 +0300

    learn Node.js

---
+++ FreeCodeCampLesson/index.js
@@ -0,0 +1,8 @@
+//for work code bellow, you need install esm package.  And run next command: node -r esm [file]
+import http from 'http';
+import 'something';
+
+//output data in console
+process.stdout.write(Math.random().toString());
+
+console.log(http);

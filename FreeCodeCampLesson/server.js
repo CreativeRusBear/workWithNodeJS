74a7bbf94588859bb3c39ac0d4c282567e562729
Artem <gusev2014russia@mail.ru>
2019-09-01 10:16:10 +0300

    run OS command in Node.js (also convert sync function into async), work with EventEmitter

--- FreeCodeCampLesson/server.js
+++ FreeCodeCampLesson/server.js
@@ -1,10 +1,8 @@
-// import http from 'http';
-// import express from 'express';
-
-
+import http from 'http';
+import express from 'express';
 
 /**
- * Using Express
+ * @summary Create server using Express
  */
 /*const server = express();
 server.get('/', (request, response)=>{
@@ -13,13 +11,14 @@ server.get('/', (request, response)=>{
 server.get('/favicon.*', (request, response)=>{
   // response.sendFile();
 });
-
-
-
 server.listen(3000);*/
 
+
+
+
+
 /**
- * Using http package
+ * @summary Create server using http package
  */
 /* const server = http.createServer();
 server.on('request', (request, response) =>{
@@ -27,12 +26,68 @@ server.on('request', (request, response) =>{
   response.write('Run server');
   response.end();
 });
-
 server.listen(3000);*/
 
 
+
+
+
 /**
- * Livereload has been added (using pnodemon package)
+ * @summary Livereload has been added (using pnodemon package)
  */
-process.stdin.resume();
-console.log('Hello world!');
+/* process.stdin.resume();
+console.log('Hello world!'); */
+
+
+
+
+
+import { exec } from 'child_process';
+
+/**
+ * @summary Run OS Windows dir command
+ */
+/*exec('dir', (error, stdout, stderr) => {
+  if (error) {
+    process.exit(1);
+  }
+  console.log(stdout);
+});*/
+
+
+
+
+
+import util from 'util';
+
+const execPromise = util.promisify(exec);
+/**
+ * @summary Run OS command and do exec function asynchronously
+ * @returns {Promise<void>}
+ */
+const main = async () =>{
+  try{
+    const { stdout } = await execPromise('dir');
+    console.log(stdout);
+  }catch (e) {
+    process.exit(1);
+  }
+};
+// main();
+
+
+
+
+
+/**
+ * @summary Listening Events
+ */
+import EventEmitter from 'events';
+
+const eventEmitter = new EventEmitter();
+eventEmitter.on('change', ()=>{
+  console.log('Something has changed');
+});
+setInterval(()=>{
+  eventEmitter.emit('change');
+}, 1000);

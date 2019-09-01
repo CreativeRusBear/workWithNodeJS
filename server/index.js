c8093cc2ee97793bc6f6a079853da57fd1d9b03f
Artem <gusev2014russia@mail.ru>
2019-09-01 11:51:50 +0300

    added new features

--- server/index.js
+++ server/index.js
@@ -6,5 +6,8 @@ let server = http.createServer((req, res) => {
     res.end('Hello world!');
 });
 
-server.listen(8000, '127.0.0.1');
-console.info('We listening port 8000');
+server.listen(8000, '127.0.0.1',()=>{
+    console.info(`Server running at: 
+    http://localhost:8000
+    http://127.0.0.1:8000`);
+});
